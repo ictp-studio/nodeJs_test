fd7029827eb75dabc9e5219f431c55485471ef04
ictp-studio <68502824+ictp-studio@users.noreply.github.com>
2026-05-03 23:05:28 +0100

    Change stream URL to use a fixed server address
    Updated the stream URL to use a fixed server address.

--- aws_update_mp_online.js
+++ aws_update_mp_online.js
@@ -1,6 +1,5 @@
 #!/usr/bin/env node
 var mountName = process.argv[2];
-var host = process.argv[3];
 
 // Load the AWS SDK for Node.js
 var AWS = require('aws-sdk');
@@ -9,6 +8,7 @@ AWS.config.update({region: 'us-east-1'});
 
 //var tableName = "minaret-dev";
 var tableName = "broadcastChannel_dev";
+const serverAddress = "http://3.84.18.186:8000";
 
 const dynamoDB = new AWS.DynamoDB.DocumentClient()
 dynamoDB
@@ -22,7 +22,7 @@ dynamoDB
     UpdateExpression: 'set current_status = :status, streamUrl = :streamUrl',
     ExpressionAttributeValues: {
       ":status": "Online",
-      ":streamUrl": host + mountName,
+      ":streamUrl" : serverAddress + mountName,
     },
   })
   .promise()
